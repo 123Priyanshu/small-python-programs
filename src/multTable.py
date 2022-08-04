95ce6696f9feeb2cd1dc590681db1c7cded61d9c
123Priyanshu <87241167+123Priyanshu@users.noreply.github.com>
2022-08-04 12:59:31 -0700

    Update multTable.py

--- src/multTable.py
+++ src/multTable.py
@@ -1,23 +1,19 @@
 def printTableRow(inputNum, rowNum):
-    Test = inputNum*rowNum
-    # Should print something like below if the inputNum is 2
-    #
-    # 2 X 1 = 2
-    # we get 2 from the inputNum
-    # we get 1 from rowNum
-    # 2 on the right hand side of = is the product of inputNum and rowNum
-    print(Test)
+    print(inputNum, 'x', rowNum, '=', inputNum*rowNum)
+# Should print something like below if the inputNum is 2
+# we get 2 from the inputNum
+# we get 1 from rowNum
+# 2 on the right hand side of = is the product of inputNum and rowNum
 
 
 def printTable(inputNum):
     a = inputNum
-    for a in range(0, 21):
-         print(inputNum,'x', a, '=', inputNum*a)
+    for a in range(0, 10):
+        print(inputNum, 'x', a, '=', inputNum*a)
+
     # Should print something like below if the inputNum is 2
-    #
     # 2 X 1 = 2
     # 2 X 2 = 4
-    # ..
     # 2 X 10 = 20
 printTableRow(5, 6)
 printTable(2)
